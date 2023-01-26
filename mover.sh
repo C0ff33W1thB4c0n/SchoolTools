4f47ea2f28d5590e831cb291d76f966ff9e1d6a5
C0ff33W1thB4c0n <fabian_stens@outlook.de>
2023-01-26 10:31:39 +0100

    +bio +zsw

--- mover.sh
+++ mover.sh
@@ -6,6 +6,8 @@ mkdir -p ~/Dokumente/school/deutsch
 mkdir -p ~/Dokumente/school/englisch
 mkdir -p ~/Dokumente/school/info
 mkdir -p ~/Dokumente/school/musik
+mkdir -p ~/Dokumente/school/biologie
+mkdir -p ~/Dokumente/school/zsw
 
 mv ~/philo* ~/Dokumente/school/philo/
 mv ~/reli* ~/Dokumente/school/reli/
@@ -13,3 +15,5 @@ mv ~/deutsch* ~/Dokumente/school/deutsch/
 mv ~/engl* ~/Dokumente/school/englisch/
 mv ~/info* ~/Dokumente/school/info/
 mv ~/musik* ~/Dokumente/school/musik/
+mv ~/bio* ~/Dokumente/school/biologie
+mv ~/zsw* ~/Dokumente/school/zsw
