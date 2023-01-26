be6e154a5df567dbfa5b7d4fb101f8a6a74ea816
C0ff33W1thB4c0n <fabian_stens@outlook.de>
2023-01-26 10:21:18 +0100

    initial commit

---
+++ mover.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# cleans up ~/ .
+mkdir -p ~/Dokumente/school/philo
+mkdir -p ~/Dokumente/school/reli
+mkdir -p ~/Dokumente/school/deutsch
+mkdir -p ~/Dokumente/school/englisch
+mkdir -p ~/Dokumente/school/info
+mkdir -p ~/Dokumente/school/musik
+
+mv ~/philo* ~/Dokumente/school/philo/
+mv ~/reli* ~/Dokumente/school/reli/
+mv ~/deutsch* ~/Dokumente/school/deutsch/
+mv ~/engl* ~/Dokumente/school/englisch/
+mv ~/info* ~/Dokumente/school/info/
+mv ~/musik* ~/Dokumente/school/musik/
---
+++ txt-pdf.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# convert (dot)txt to (dot)pdf
+
+unoconv ./$1
