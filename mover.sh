7455f422d9c43dcc1e49fced4941e10e968d424a
C0ff33W1thB4c0n <fabian_stens@outlook.de>
2023-01-27 11:10:23 +0100

    +setup.sh; tried sth in mover

--- mover.sh
+++ mover.sh
@@ -1,23 +1,32 @@
-#!/bin/bashi
+#!/bin/bash
 
 # @author	C0ff33W1thB4c0n	<fabian_stens@outlook.de>
 # Licensed under WTFPL(c)2023
 
 # cleans up ~/ .
-mkdir -p ~/Dokumente/school/philo
-mkdir -p ~/Dokumente/school/reli
-mkdir -p ~/Dokumente/school/deutsch
-mkdir -p ~/Dokumente/school/englisch
-mkdir -p ~/Dokumente/school/info
-mkdir -p ~/Dokumente/school/musik
-mkdir -p ~/Dokumente/school/biologie
-mkdir -p ~/Dokumente/school/zsw
+moving() {
+	mkdir -p ~/Dokumente/school/philo
+	mkdir -p ~/Dokumente/school/reli
+	mkdir -p ~/Dokumente/school/deutsch
+	mkdir -p ~/Dokumente/school/englisch
+	mkdir -p ~/Dokumente/school/info
+	mkdir -p ~/Dokumente/school/musik
+	mkdir -p ~/Dokumente/school/biologie
+	mkdir -p ~/Dokumente/school/zsw
 
-mv ~/philo* ~/Dokumente/school/philo/
-mv ~/reli* ~/Dokumente/school/reli/
-mv ~/deutsch* ~/Dokumente/school/deutsch/
-mv ~/engl* ~/Dokumente/school/englisch/
-mv ~/info* ~/Dokumente/school/info/
-mv ~/musik* ~/Dokumente/school/musik/
-mv ~/bio* ~/Dokumente/school/biologie
-mv ~/zsw* ~/Dokumente/school/zsw
+	mv ~/philo* ~/Dokumente/school/philo/
+	mv ~/reli* ~/Dokumente/school/reli/
+	mv ~/deutsch* ~/Dokumente/school/deutsch/
+	mv ~/engl* ~/Dokumente/school/englisch/
+	mv ~/info* ~/Dokumente/school/info/
+	mv ~/musik* ~/Dokumente/school/musik/
+	mv ~/bio* ~/Dokumente/school/biologie
+	mv ~/zsw* ~/Dokumente/school/zsw
+}
+
+if [[ $1=="-d" ]]
+then
+	moving
+	exit 0
+fi
+moving > /dev/null 2>&1
---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# setup.sh
+
+# C0ff33W1thB4c0n	<fabian_stens@outlook.de>
+# Licensed under WTFPL(c)2023
+
+# copies mover.sh (later also txt-pdf.sh) into /bin
+sudo cp ./mover.sh /bin/mover
